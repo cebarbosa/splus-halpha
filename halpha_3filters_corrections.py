47195c75b112b90acb4f39ea8f8ef5798c3f709a
cebarbosa <kadu.barbosa@gmail.com>
2021-06-25 15:33:37 -0300

    Producing halpha images for Fornax galaxies.

--- halpha_3filters_corrections.py
+++ halpha_3filters_corrections.py
@@ -3,23 +3,14 @@
 Author : Carlos Eduardo Barbosa
 Co-author: Jessica Silva Amorim
 
-Methods to extract H-alpha and 
-study the Star Formation Rate 
-with SPLUS filters according 
-to methods presented 
-in Vilella-Rojo (2015) 
+Methods to extract H-alpha and study the Star Formation Rate with SPLUS filters
+according to methods presented in Vilella-Rojo (2015)
 
 """
-
 import os
 
-
-import matplotlib.pyplot as plt
-
-import extinction
 import astropy.units as u
 from astropy.io import fits
-from astropy.wcs import WCS
 from astropy.table import Table
 import matplotlib.pyplot as plt
 import numpy as np
@@ -27,47 +18,46 @@ import numpy as np
 import context
 from test_halpha import get_names
 
-
 def calc_halpha_nii_3F(fnu):
     """ Calculates Halpha with 3 bands using eq. 3 of Vilella-Rojo+ (2015). """
     fnu_F660 = fnu[1]
     fnu_r = fnu[0]
     fnu_i = fnu[2]
-    fnu_F660_corr = 2 * fnu_F660
-
-    coef_file = os.path.join(context.tables_dir, "coeffs.fits")
-    COEFS = fits.open(coef_file)
-
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    coef_file = os.path.join(dir_path, "coeffs.csv")
     t = Table.read(coef_file)
     alpha_F660 = t['alpha_x'][0]
     beta_F660 = t['beta_x'][0]
-    delta_F660 = t['delta_x'][0]
-
     alpha_i = t['alpha_x'][1]
-    beta_i = t['beta_x'][1]
-    delta_i = i = t['delta_x'][1]
-
     alpha_r = t['alpha_x'][2]
     beta_r = t['beta_x'][2]
-    delta_r = t['delta_x'][2]
-
     a = (alpha_r - alpha_i) / (alpha_F660 - alpha_i)
     numen = (fnu_r - fnu_i) - a * (fnu_F660 - fnu_i)
     denom = - a * beta_F660 + beta_r
     return numen / denom
 
-
-def calc_halpha_without_nii(halpha_nii, g_i): 
+def nii_correction(halpha_nii, g_i):
     """ Calculated NII corrected emission with eq. 21 or Vilella-Rojo+ (2015)"""
     idx = np.where(halpha_nii < 0)
     halpha = np.power(10, np.where((g_i <= 0.5),
                           0.989 * np.log10(halpha_nii) - 0.193,
                           0.954 * np.log10(halpha_nii) - 0.753))
     halpha[idx] = halpha_nii[idx]
-    corr = halpha / halpha_nii
     return halpha
 
-def process_galaxies():
+def dust_correction(halpha, g_i):
+    wave = 6614.0 * u.Angstrom
+    """" C = E(B-V) extinction law  with  eq. 20 or Vilella-Rojo+ (2015)"""
+    ebv = np.array(0.206 * np.power(g_i, 1.68) - 0.0457)
+    ebv[np.isnan(ebv)] = 0
+    x = 1 / wave.to(u.micrometer).value
+    wtran = (0.63 * u.micrometer)
+    kappa = np.where(wave > wtran, 2.659 * (-1.857 + 1.040 * x),
+                     2.659 * (-2.156 + 1.509 * x - 0.198 * x * x
+                              + 0.011 * (x * x * x)))
+    return halpha * np.power(10, -0.4 * ebv * kappa)
+
+def test_halpha():
     data_dir = os.path.join(context.data_dir, "11HUGS/cutouts")
     galaxies = os.listdir(data_dir)
     for galaxy in galaxies:
@@ -80,30 +70,18 @@ def process_galaxies():
         zero_point = np.array(
             [fits.getval(name, "MAGZP", ext = 1) for name in imgnames])
         fnu = data * np.power(10, - 0.4 * (zero_point[:, None, None] + 48.6))
-        halpha_nii = calc_halpha_nii_3F(fnu)
+        halpha_nii_dust = calc_halpha_nii_3F(fnu)
         magAB = -2.5 * np.log10(fnu) - 48.6
         g_i = magAB[3] - magAB[2]
-        wave = 6614.0 * u.Angstrom
-        """" C = E(B-V) extinction law  with  eq. 20 or Vilella-Rojo+ (2015)"""
-        ebv = np.array(0.206 * np.power(g_i, 1.68) - 0.0457)
-        ebv[np.isnan(ebv)] = 0
-        x = 1 / wave.to(u.micrometer).value
-        wtran = (0.63 * u.micrometer)
-        kappa = np.where(wave > wtran, 2.659 * (-1.857 + 1.040 * x),
-                               2.659 * (-2.156 + 1.509 * x - 0.198 * x * x
-                                       + 0.011 * (x * x * x)))
-        """" Extinction.calzetti00 with A_V = R_V * c and R_V = 4.05 +-0.80 """
-        dust_correction = np.power(10, -0.4 * ebv * kappa)
-        halpha_nii_corr = halpha_nii * dust_correction
-        halpha = calc_halpha_without_nii(halpha_nii_corr, g_i)
-        
-        vmax = np.percentile(halpha_nii, 95)
-        vmin = np.percentile(halpha_nii, 10)
+        halpha_nii = dust_correction(halpha_nii_dust, g_i)
+        halpha = nii_correction(halpha_nii, g_i)
+        vmax = np.percentile(halpha_nii_dust, 95)
+        vmin = np.percentile(halpha_nii_dust, 10)
         plt.subplot(1,3,1)
-        plt.imshow(halpha_nii, vmax=vmax, vmin=vmin, origin="lower")
+        plt.imshow(halpha_nii_dust, vmax=vmax, vmin=vmin, origin="lower")
         plt.colorbar()
         plt.subplot(1,3,2)
-        plt.imshow(halpha_nii_corr, vmax=vmax, vmin=vmin, origin="lower")
+        plt.imshow(halpha_nii, vmax=vmax, vmin=vmin, origin="lower")
         plt.colorbar()
         plt.subplot(1,3,3)
         
@@ -113,6 +91,5 @@ def process_galaxies():
         plt.colorbar()
         plt.show()
 
-
 if __name__ == "__main__":
-    process_galaxies()
+    test_halpha()
---
+++ make_halpha_fornax.py
@@ -0,0 +1,72 @@
+import os
+import getpass  # For authentication
+
+import numpy as np
+import astropy.units as u
+from astropy.io import fits
+from astropy.table import Table
+import matplotlib.pyplot as plt
+import splusdata  # To access the S-PLUS database
+from splus_ifusci import SCube, make_RGB_with_overlay
+
+import context
+from halpha_3filters_corrections import dust_correction, nii_correction
+
+if __name__ == "__main__":
+    wdir = os.path.join(context.data_dir, "FCC_halpha")
+    tablename = os.path.join(context.home_dir,
+                         "tables/Fornax_Sample_3arcsec_pz_zs_class_xmatch.csv")
+    table = Table.read(tablename)
+    galaxies = table["ID"].data
+    ra = table["RA"].data
+    dec = table["DEC"].data
+    coords = [[r,d] for r,d in zip(ra, dec)]
+    if not os.path.exists(wdir):
+        os.mkdir(wdir)
+    # Specifying your object
+    # galaxies = ['NGC1087', 'NGC1087']
+    # coords = [['02:46:25.15', '-00:29:55.45'],
+    #           ['02:46:25.15', '-00:29:55.45']]
+    sizes = [256] * len(galaxies)  # Assume pixels if units is not specified
+    # Connect with S-PLUS
+    username = getpass.getuser()  # Change to your S-PLUS username
+    password = getpass.getpass(f"Password for {username}:")
+    conn = splusdata.connect(username, password)
+    # conn = None
+    for galaxy, coord, size in zip(galaxies, coords, sizes):
+        gal_dir = os.path.join(wdir, galaxy)
+        if not os.path.exists(gal_dir):
+            os.mkdir(gal_dir)
+        halpha_img = os.path.join(gal_dir, f"{galaxy}_{size}x"
+                                        f"{size}pix_halpha.fits")
+        # Main routine to download the datacube.
+        scube = SCube(galaxy, coord, size, conn=conn,
+                      coord_unit=(u.degree, u.degree), wdir=gal_dir)
+        scube.download_stamps()
+        scube.make_cube()
+        # Processing the data
+        mag = scube.get_mag()
+        magerr = scube.get_magerr()
+        idx_g = scube.bands.index("G")
+        idx_i = scube.bands.index("I")
+        g_i = mag[idx_g] - mag[idx_i]
+        halpha_obs, halpha_obs_err = scube.calc_halpha(store=True)
+        halpha_nii = dust_correction(halpha_obs.value, g_i)
+        halpha_nii_err = dust_correction(halpha_obs_err.value, g_i)
+        halpha = nii_correction(halpha_nii, g_i)
+        halpha_err = nii_correction(halpha_nii_err, g_i)
+        # Saving fits
+        output = scube.cubename.replace(".fits", "_halpha.fits")
+        h = fits.getheader(os.path.join(scube.cutouts_dir, scube.cutnames[0]),
+                           ext=1)
+        h["EXTNAME"] = "DATA"
+        hdu1 = fits.ImageHDU(halpha, h)
+        h["EXTNAME"] = "ERROR"
+        hdu2 = fits.ImageHDU(halpha_err, h)
+        hdulist = fits.HDUList([fits.PrimaryHDU(), hdu1, hdu2])
+        hdulist.writeto(output, overwrite=True)
+        flam = scube.get_flam().value
+        rgb_bands = ["I", "R", "G"]
+        rgb = [flam[scube.bands.index(b)] for b in rgb_bands]
+        outimg = os.path.join(gal_dir, f"{galaxy}_RGB.png")
+        make_RGB_with_overlay(*rgb, outimg, overlay=halpha_obs)
