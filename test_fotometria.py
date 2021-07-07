5407fb0f2934e98befe5995420c12ef9f71b6933
Jessica Amorim <72164735+Jessica-amorim@users.noreply.github.com>
2021-07-07 17:30:01 -0300

    Add files via upload

--- test_fotometria.py
+++ test_fotometria.py
@@ -19,41 +19,36 @@ from photutils import aperture_photometry
 from photutils.datasets import make_100gaussians_image
 from photutils import CircularAperture, CircularAnnulus
 
-def get_names(wdir):
-# wdir=pasta+nome da galaxia
-    """ Obtaining names of images  for one directory. """
-    filenames = [x for x in os.listdir(wdir) if x.endswith("_swp.fits")]
-    names = []
-    for band in ["R"]:
-        names.append([x for x in filenames if x.split("_")[2]==band][0])
-    return names
-
-data_dir = os.path.join(context.data_dir, "11HUGS/cutouts")
-galaxies = os.listdir(data_dir)
+data_dir = os.path.join(context.data_dir, "FCC_halpha")
+galaxies = os.listdir(data_dir) 
 
 for galaxy in galaxies:
     wdir = os.path.join(data_dir, galaxy)
     os.chdir(wdir)
-    imgnames = get_names(wdir)
+    cubename = [x for x in os.listdir(wdir) if x.endswith("pix.fits")][0]
     # Loading data
-    data = fits.getdata(imgnames[0], ext=1)
-    
+    data = fits.getdata(cubename, ext=1)
+    t = Table.read(cubename)
+    t['FILTER'].tolist()
+    idx = t['FILTER'].tolist().index('R')
+    rband = data[idx, :, : ]
+   
     #Creating Aperture Objects
-    positions =  np.array([0.5 * data.shape[0], 0.5 * data.shape[1]])
-    radii = np.linspace(1, data.shape[0] / 2., 30)
+    positions =  np.array([0.5 * rband.shape[0], 0.5 * rband.shape[1]])
+    radii = np.linspace(1, rband.shape[0] / 2., 30)
     # apertures = [CircularAperture(positions, r=r) for r in radii]
     apertures = []
     for r in radii:
         aperture = CircularAperture(positions, r=r)
         apertures.append(aperture)
     # Performing Aperture Photometry
-    phot_table = aperture_photometry(data, apertures)
+    phot_table = aperture_photometry(rband, apertures)
     # Lendo os valores da table
     phot = [float(phot_table["aperture_sum_{}".format(i)]) for i in range(30)]
     plt.plot(radii, phot, "o")
+    plt.savefig('CUBE_FOTOMETRIA_R.png')
     plt.show()
-
-     
+    
     # #Multiple Apertures at Each Position
     # radii = [3., 4., 5.]
     # apertures = [CircularAperture(positions, r=r) for r in radii]
